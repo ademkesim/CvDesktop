08a353c721ece823bdd3cd3d86a487dcc623d3e6
ademkesim <48206386+ademkesim@users.noreply.github.com>
2020-03-13 11:14:07 +0300

    formbasvur panelinde değişiklikler uygulandı

--- deneme/PoldyCv/formBasvur.Designer.cs
+++ deneme/PoldyCv/formBasvur.Designer.cs
@@ -29,22 +29,18 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(formBasvur));
-            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
-            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.panel1 = new System.Windows.Forms.Panel();
-            this.mskTel = new System.Windows.Forms.MaskedTextBox();
             this.btnCvIndir = new System.Windows.Forms.Button();
             this.btnCvDuzenle = new System.Windows.Forms.Button();
             this.btnCvOlustur = new System.Windows.Forms.Button();
             this.picProfile = new System.Windows.Forms.PictureBox();
-            this.label5 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.txtPosta = new System.Windows.Forms.TextBox();
-            this.txtMeslek = new System.Windows.Forms.TextBox();
             this.txtSoyad = new System.Windows.Forms.TextBox();
             this.txtAd = new System.Windows.Forms.TextBox();
             this.grafikDepartman = new System.Windows.Forms.DataVisualization.Charting.Chart();
@@ -61,18 +57,14 @@
             // 
             // panel1
             // 
-            this.panel1.Controls.Add(this.mskTel);
             this.panel1.Controls.Add(this.btnCvIndir);
             this.panel1.Controls.Add(this.btnCvDuzenle);
             this.panel1.Controls.Add(this.btnCvOlustur);
             this.panel1.Controls.Add(this.picProfile);
-            this.panel1.Controls.Add(this.label5);
             this.panel1.Controls.Add(this.label4);
-            this.panel1.Controls.Add(this.label3);
             this.panel1.Controls.Add(this.label2);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.txtPosta);
-            this.panel1.Controls.Add(this.txtMeslek);
             this.panel1.Controls.Add(this.txtSoyad);
             this.panel1.Controls.Add(this.txtAd);
             this.panel1.Location = new System.Drawing.Point(35, 39);
@@ -80,23 +72,14 @@
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(412, 538);
             this.panel1.TabIndex = 0;
-            // 
-            // mskTel
-            // 
-            this.mskTel.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.mskTel.Location = new System.Drawing.Point(131, 377);
-            this.mskTel.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.mskTel.Mask = "(999) 000-0000";
-            this.mskTel.Name = "mskTel";
-            this.mskTel.Size = new System.Drawing.Size(231, 36);
-            this.mskTel.TabIndex = 5;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
             // btnCvIndir
             // 
             this.btnCvIndir.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(130)))), ((int)(((byte)(49)))));
             this.btnCvIndir.Font = new System.Drawing.Font("Calibri", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnCvIndir.ForeColor = System.Drawing.Color.White;
-            this.btnCvIndir.Location = new System.Drawing.Point(265, 466);
+            this.btnCvIndir.Location = new System.Drawing.Point(263, 439);
             this.btnCvIndir.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnCvIndir.Name = "btnCvIndir";
             this.btnCvIndir.Size = new System.Drawing.Size(117, 39);
@@ -110,7 +93,7 @@
             this.btnCvDuzenle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(254)))), ((int)(((byte)(211)))), ((int)(((byte)(48)))));
             this.btnCvDuzenle.Font = new System.Drawing.Font("Calibri", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnCvDuzenle.ForeColor = System.Drawing.Color.Black;
-            this.btnCvDuzenle.Location = new System.Drawing.Point(141, 466);
+            this.btnCvDuzenle.Location = new System.Drawing.Point(139, 439);
             this.btnCvDuzenle.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnCvDuzenle.Name = "btnCvDuzenle";
             this.btnCvDuzenle.Size = new System.Drawing.Size(117, 39);
@@ -123,7 +106,7 @@
             this.btnCvOlustur.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(69)))), ((int)(((byte)(170)))), ((int)(((byte)(242)))));
             this.btnCvOlustur.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnCvOlustur.ForeColor = System.Drawing.Color.White;
-            this.btnCvOlustur.Location = new System.Drawing.Point(17, 466);
+            this.btnCvOlustur.Location = new System.Drawing.Point(13, 439);
             this.btnCvOlustur.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.btnCvOlustur.Name = "btnCvOlustur";
             this.btnCvOlustur.Size = new System.Drawing.Size(117, 39);
@@ -134,7 +117,7 @@
             // picProfile
             // 
             this.picProfile.Image = ((System.Drawing.Image)(resources.GetObject("picProfile.Image")));
-            this.picProfile.Location = new System.Drawing.Point(167, 62);
+            this.picProfile.Location = new System.Drawing.Point(139, 54);
             this.picProfile.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.picProfile.Name = "picProfile";
             this.picProfile.Size = new System.Drawing.Size(155, 129);
@@ -142,41 +125,21 @@
             this.picProfile.TabIndex = 10;
             this.picProfile.TabStop = false;
             // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(29, 384);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(94, 29);
-            this.label5.TabIndex = 9;
-            this.label5.Text = "Telefon:";
-            // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(29, 342);
+            this.label4.Location = new System.Drawing.Point(37, 332);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(95, 29);
             this.label4.TabIndex = 8;
             this.label4.Text = "E-Posta:";
             // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(32, 300);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(93, 29);
-            this.label3.TabIndex = 7;
-            this.label3.Text = "Meslek:";
-            // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(45, 258);
+            this.label2.Location = new System.Drawing.Point(53, 287);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(80, 29);
             this.label2.TabIndex = 6;
@@ -186,7 +149,7 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(77, 217);
+            this.label1.Location = new System.Drawing.Point(85, 246);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(48, 29);
             this.label1.TabIndex = 5;
@@ -196,25 +159,16 @@
             // txtPosta
             // 
             this.txtPosta.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtPosta.Location = new System.Drawing.Point(131, 335);
+            this.txtPosta.Location = new System.Drawing.Point(139, 325);
             this.txtPosta.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtPosta.Name = "txtPosta";
             this.txtPosta.Size = new System.Drawing.Size(231, 36);
             this.txtPosta.TabIndex = 4;
             // 
-            // txtMeslek
-            // 
-            this.txtMeslek.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtMeslek.Location = new System.Drawing.Point(131, 293);
-            this.txtMeslek.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.txtMeslek.Name = "txtMeslek";
-            this.txtMeslek.Size = new System.Drawing.Size(231, 36);
-            this.txtMeslek.TabIndex = 3;
-            // 
             // txtSoyad
             // 
             this.txtSoyad.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtSoyad.Location = new System.Drawing.Point(131, 251);
+            this.txtSoyad.Location = new System.Drawing.Point(139, 280);
             this.txtSoyad.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtSoyad.Name = "txtSoyad";
             this.txtSoyad.Size = new System.Drawing.Size(231, 36);
@@ -223,7 +177,7 @@
             // txtAd
             // 
             this.txtAd.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtAd.Location = new System.Drawing.Point(131, 209);
+            this.txtAd.Location = new System.Drawing.Point(139, 238);
             this.txtAd.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.txtAd.Name = "txtAd";
             this.txtAd.Size = new System.Drawing.Size(231, 36);
@@ -231,17 +185,17 @@
             // 
             // grafikDepartman
             // 
-            chartArea1.Name = "ChartArea1";
-            this.grafikDepartman.ChartAreas.Add(chartArea1);
-            legend1.Name = "Legend1";
-            this.grafikDepartman.Legends.Add(legend1);
+            chartArea2.Name = "ChartArea1";
+            this.grafikDepartman.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.grafikDepartman.Legends.Add(legend2);
             this.grafikDepartman.Location = new System.Drawing.Point(477, 84);
             this.grafikDepartman.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.grafikDepartman.Name = "grafikDepartman";
-            series1.ChartArea = "ChartArea1";
-            series1.Legend = "Legend1";
-            series1.Name = "Aranan Eleman Sayisi";
-            this.grafikDepartman.Series.Add(series1);
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Aranan Eleman Sayisi";
+            this.grafikDepartman.Series.Add(series2);
             this.grafikDepartman.Size = new System.Drawing.Size(742, 229);
             this.grafikDepartman.TabIndex = 1;
             this.grafikDepartman.Text = "chart1";
@@ -337,19 +291,15 @@
 
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.PictureBox picProfile;
-        private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox txtPosta;
-        private System.Windows.Forms.TextBox txtMeslek;
         private System.Windows.Forms.TextBox txtSoyad;
         private System.Windows.Forms.TextBox txtAd;
         private System.Windows.Forms.Button btnCvIndir;
         private System.Windows.Forms.Button btnCvDuzenle;
         private System.Windows.Forms.Button btnCvOlustur;
-        private System.Windows.Forms.MaskedTextBox mskTel;
         private System.Windows.Forms.DataVisualization.Charting.Chart grafikDepartman;
         private System.Windows.Forms.RichTextBox richTextBox1;
         private System.Windows.Forms.ComboBox comboBox1;
