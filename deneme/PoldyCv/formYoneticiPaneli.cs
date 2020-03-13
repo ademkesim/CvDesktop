dced3fafe9e1e94ad22095914989eb95ed7fa8be
kaktimurf <57290668+kaktimurf@users.noreply.github.com>
2020-03-13 11:28:12 +0300

    Yönetici Paneli

--- deneme/PoldyCv/formYoneticiPaneli.cs
+++ deneme/PoldyCv/formYoneticiPaneli.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+	
+
 
 namespace PoldyCv
 {
@@ -17,9 +20,27 @@ namespace PoldyCv
             InitializeComponent();
         }
 
+        Baglanti bgl = new Baglanti();
         private void formYoneticiPanel_Load(object sender, EventArgs e)
         {
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter("Select * From Cv_Basvuru ", bgl.baglanti());
+            da.Fill(dt);
+            dataBasvurular.DataSource = dt;
 
+            SqlCommand komut3 = new SqlCommand("Select count(*) From Cv_Basvuru",bgl.baglanti());
+            SqlDataReader dr3 = komut3.ExecuteReader();
+            while (dr3.Read())
+            {
+                lblToplamBasvuruSayisi.Text = dr3[0].ToString();
+            }
+
+            SqlCommand komut4 = new SqlCommand("Select KisiResim From Cv_Kisiler where kisiid=1",bgl.baglanti());
+            SqlDataReader dr4 = komut4.ExecuteReader();
+            if (dr4.Read())
+            {
+                picProfile.ImageLocation= dr4[0].ToString();
+            }
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
@@ -46,5 +67,35 @@ namespace PoldyCv
         {
 
         }
+
+        private void btnIlanEkle_Click(object sender, EventArgs e)
+        {
+           
+            SqlCommand komut2 = new SqlCommand("Update Cv_Departmanlar set DepartmanAd=@p1,Departmanihtiyaç+=@p2",bgl.baglanti());
+            komut2.Parameters.AddWithValue("@p1", cmbDepartmanSec.Text);
+            komut2.Parameters.AddWithValue("@p2",txtKacKisi.Text);
+            komut2.ExecuteNonQuery();
+            MessageBox.Show(txtKacKisi.Text+"Yeni iş ilanı eklendi","Bilgi",MessageBoxButtons.OK,MessageBoxIcon.Information);
+            bgl.baglanti().Close();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cmbDepartmanSec_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SqlCommand komut1 = new SqlCommand("Select Departmanihtiyaç From Cv_Departmanlar Where DepartmanAd=@p1",bgl.baglanti());
+            komut1.Parameters.AddWithValue("@p1", cmbDepartmanSec.Text);
+            SqlDataReader dr = komut1.ExecuteReader();
+            while(dr.Read())
+            {
+                label1.Text = dr[0].ToString();
+            }
+               
+           
+            bgl.baglanti().Close();
+        }    
     }
 }
--- deneme/PoldyCv/formYoneticiPaneli.designer.cs
+++ deneme/PoldyCv/formYoneticiPaneli.designer.cs
@@ -30,6 +30,9 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(formYoneticiPaneli));
             this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtKacKisi = new System.Windows.Forms.TextBox();
+            this.cmbDepartmanSec = new System.Windows.Forms.ComboBox();
             this.btnIlanEkle = new System.Windows.Forms.Button();
             this.picLogo = new System.Windows.Forms.PictureBox();
             this.lblMerhaba = new System.Windows.Forms.Label();
@@ -39,7 +42,6 @@
             this.label6 = new System.Windows.Forms.Label();
             this.lblToplamBasvuruSayisi = new System.Windows.Forms.Label();
             this.cmbUnvan = new System.Windows.Forms.ComboBox();
-            this.cmbDepartmanSec = new System.Windows.Forms.ComboBox();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.picLogo)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.picProfile)).BeginInit();
@@ -49,34 +51,79 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.Transparent;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.txtKacKisi);
             this.panel1.Controls.Add(this.cmbDepartmanSec);
             this.panel1.Controls.Add(this.btnIlanEkle);
             this.panel1.Controls.Add(this.picLogo);
             this.panel1.Controls.Add(this.lblMerhaba);
             this.panel1.Controls.Add(this.picProfile);
-            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Location = new System.Drawing.Point(9, 10);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(902, 239);
+            this.panel1.Size = new System.Drawing.Size(676, 194);
             this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(303, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 20);
+            this.label1.TabIndex = 11;
+            // 
+            // txtKacKisi
+            // 
+            this.txtKacKisi.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtKacKisi.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.txtKacKisi.Location = new System.Drawing.Point(151, 60);
+            this.txtKacKisi.Name = "txtKacKisi";
+            this.txtKacKisi.Size = new System.Drawing.Size(138, 27);
+            this.txtKacKisi.TabIndex = 10;
+            this.txtKacKisi.Text = "Kaç Kişilik";
+            this.txtKacKisi.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // cmbDepartmanSec
+            // 
+            this.cmbDepartmanSec.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbDepartmanSec.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.cmbDepartmanSec.FormattingEnabled = true;
+            this.cmbDepartmanSec.Items.AddRange(new object[] {
+            "İnsan Kaynakları",
+            "Bt",
+            "Üretim",
+            "Satın Alma ",
+            "Muhasebe"});
+            this.cmbDepartmanSec.Location = new System.Drawing.Point(151, 28);
+            this.cmbDepartmanSec.Margin = new System.Windows.Forms.Padding(2);
+            this.cmbDepartmanSec.Name = "cmbDepartmanSec";
+            this.cmbDepartmanSec.Size = new System.Drawing.Size(138, 27);
+            this.cmbDepartmanSec.TabIndex = 9;
+            this.cmbDepartmanSec.Text = "Departman Seç";
+            this.cmbDepartmanSec.SelectedIndexChanged += new System.EventHandler(this.cmbDepartmanSec_SelectedIndexChanged);
             // 
             // btnIlanEkle
             // 
             this.btnIlanEkle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(75)))), ((int)(((byte)(101)))), ((int)(((byte)(132)))));
             this.btnIlanEkle.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnIlanEkle.ForeColor = System.Drawing.Color.White;
-            this.btnIlanEkle.Location = new System.Drawing.Point(201, 76);
+            this.btnIlanEkle.Location = new System.Drawing.Point(151, 91);
+            this.btnIlanEkle.Margin = new System.Windows.Forms.Padding(2);
             this.btnIlanEkle.Name = "btnIlanEkle";
-            this.btnIlanEkle.Size = new System.Drawing.Size(183, 44);
+            this.btnIlanEkle.Size = new System.Drawing.Size(137, 36);
             this.btnIlanEkle.TabIndex = 8;
             this.btnIlanEkle.Text = "İş İlanı Ekle";
             this.btnIlanEkle.UseVisualStyleBackColor = false;
+            this.btnIlanEkle.Click += new System.EventHandler(this.btnIlanEkle_Click);
             // 
             // picLogo
             // 
             this.picLogo.Image = ((System.Drawing.Image)(resources.GetObject("picLogo.Image")));
-            this.picLogo.Location = new System.Drawing.Point(410, 15);
+            this.picLogo.Location = new System.Drawing.Point(364, 12);
+            this.picLogo.Margin = new System.Windows.Forms.Padding(2);
             this.picLogo.Name = "picLogo";
-            this.picLogo.Size = new System.Drawing.Size(478, 205);
+            this.picLogo.Size = new System.Drawing.Size(302, 167);
             this.picLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.picLogo.TabIndex = 7;
             this.picLogo.TabStop = false;
@@ -85,18 +132,20 @@
             // 
             this.lblMerhaba.AutoSize = true;
             this.lblMerhaba.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblMerhaba.Location = new System.Drawing.Point(13, 169);
+            this.lblMerhaba.Location = new System.Drawing.Point(10, 137);
+            this.lblMerhaba.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblMerhaba.Name = "lblMerhaba";
-            this.lblMerhaba.Size = new System.Drawing.Size(156, 29);
+            this.lblMerhaba.Size = new System.Drawing.Size(121, 23);
             this.lblMerhaba.TabIndex = 1;
             this.lblMerhaba.Text = "Merhaba Tarık";
             // 
             // picProfile
             // 
             this.picProfile.Image = ((System.Drawing.Image)(resources.GetObject("picProfile.Image")));
-            this.picProfile.Location = new System.Drawing.Point(18, 34);
+            this.picProfile.Location = new System.Drawing.Point(14, 28);
+            this.picProfile.Margin = new System.Windows.Forms.Padding(2);
             this.picProfile.Name = "picProfile";
-            this.picProfile.Size = new System.Drawing.Size(147, 119);
+            this.picProfile.Size = new System.Drawing.Size(110, 97);
             this.picProfile.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.picProfile.TabIndex = 0;
             this.picProfile.TabStop = false;
@@ -105,31 +154,40 @@
             // 
             this.dataBasvurular.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(154)))), ((int)(((byte)(236)))), ((int)(((byte)(219)))));
             this.dataBasvurular.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataBasvurular.Location = new System.Drawing.Point(12, 299);
+            this.dataBasvurular.Location = new System.Drawing.Point(9, 243);
+            this.dataBasvurular.Margin = new System.Windows.Forms.Padding(2);
             this.dataBasvurular.Name = "dataBasvurular";
             this.dataBasvurular.RowHeadersWidth = 51;
             this.dataBasvurular.RowTemplate.Height = 24;
-            this.dataBasvurular.Size = new System.Drawing.Size(902, 296);
+            this.dataBasvurular.Size = new System.Drawing.Size(676, 240);
             this.dataBasvurular.TabIndex = 1;
             // 
             // cmbDepartman
             // 
             this.cmbDepartman.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cmbDepartman.FormattingEnabled = true;
-            this.cmbDepartman.Location = new System.Drawing.Point(12, 257);
+            this.cmbDepartman.Items.AddRange(new object[] {
+            "İnsan Kaynakları",
+            "Bt",
+            "Üretim",
+            "Satın Alma ",
+            "Muhasebe"});
+            this.cmbDepartman.Location = new System.Drawing.Point(9, 209);
+            this.cmbDepartman.Margin = new System.Windows.Forms.Padding(2);
             this.cmbDepartman.Name = "cmbDepartman";
-            this.cmbDepartman.Size = new System.Drawing.Size(221, 36);
+            this.cmbDepartman.Size = new System.Drawing.Size(167, 30);
             this.cmbDepartman.TabIndex = 2;
-            this.cmbDepartman.Text = "Tümü";
+            this.cmbDepartman.Text = "Departman ID";
             this.cmbDepartman.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // label6
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(623, 260);
+            this.label6.Location = new System.Drawing.Point(467, 211);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(236, 29);
+            this.label6.Size = new System.Drawing.Size(183, 23);
             this.label6.TabIndex = 3;
             this.label6.Text = "Toplam Başvuru Sayısı:";
             // 
@@ -137,46 +195,42 @@
             // 
             this.lblToplamBasvuruSayisi.AutoSize = true;
             this.lblToplamBasvuruSayisi.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblToplamBasvuruSayisi.Location = new System.Drawing.Point(865, 260);
+            this.lblToplamBasvuruSayisi.Location = new System.Drawing.Point(649, 211);
+            this.lblToplamBasvuruSayisi.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblToplamBasvuruSayisi.Name = "lblToplamBasvuruSayisi";
-            this.lblToplamBasvuruSayisi.Size = new System.Drawing.Size(49, 29);
+            this.lblToplamBasvuruSayisi.Size = new System.Drawing.Size(0, 23);
             this.lblToplamBasvuruSayisi.TabIndex = 4;
-            this.lblToplamBasvuruSayisi.Text = "100";
             // 
             // cmbUnvan
             // 
             this.cmbUnvan.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cmbUnvan.FormattingEnabled = true;
-            this.cmbUnvan.Location = new System.Drawing.Point(239, 257);
+            this.cmbUnvan.Items.AddRange(new object[] {
+            "İnsan Uzmanı",
+            "Bt Uzmanı",
+            "Personel",
+            "Bilgisayar Mühendisi"});
+            this.cmbUnvan.Location = new System.Drawing.Point(179, 209);
+            this.cmbUnvan.Margin = new System.Windows.Forms.Padding(2);
             this.cmbUnvan.Name = "cmbUnvan";
-            this.cmbUnvan.Size = new System.Drawing.Size(221, 36);
+            this.cmbUnvan.Size = new System.Drawing.Size(167, 30);
             this.cmbUnvan.TabIndex = 5;
-            this.cmbUnvan.Text = "Tümü";
-            // 
-            // cmbDepartmanSec
-            // 
-            this.cmbDepartmanSec.Font = new System.Drawing.Font("Calibri", 13.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.cmbDepartmanSec.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.cmbDepartmanSec.FormattingEnabled = true;
-            this.cmbDepartmanSec.Location = new System.Drawing.Point(201, 34);
-            this.cmbDepartmanSec.Name = "cmbDepartmanSec";
-            this.cmbDepartmanSec.Size = new System.Drawing.Size(183, 36);
-            this.cmbDepartmanSec.TabIndex = 9;
-            this.cmbDepartmanSec.Text = "Departman Seç";
+            this.cmbUnvan.Text = "Ünvan Seç";
             // 
             // formYoneticiPaneli
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(209)))), ((int)(((byte)(216)))), ((int)(((byte)(224)))));
-            this.ClientSize = new System.Drawing.Size(926, 617);
+            this.ClientSize = new System.Drawing.Size(694, 501);
             this.Controls.Add(this.cmbUnvan);
             this.Controls.Add(this.lblToplamBasvuruSayisi);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.cmbDepartman);
             this.Controls.Add(this.dataBasvurular);
             this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.MaximizeBox = false;
             this.Name = "formYoneticiPaneli";
             this.Text = "Yönetici Paneli";
@@ -204,5 +258,7 @@
         private System.Windows.Forms.PictureBox picLogo;
         private System.Windows.Forms.Button btnIlanEkle;
         private System.Windows.Forms.ComboBox cmbDepartmanSec;
+        private System.Windows.Forms.TextBox txtKacKisi;
+        private System.Windows.Forms.Label label1;
     }
 }
