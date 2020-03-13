9e26b227392f3389143884df66f459e469407dd6
ademkesim <48206386+ademkesim@users.noreply.github.com>
2020-03-13 10:26:49 +0300

    pdf indir butonu aktif hale getirildi

--- deneme/PoldyCv/formBasvur.cs
+++ deneme/PoldyCv/formBasvur.cs
@@ -8,7 +8,10 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
-
+using iTextSharp.text.pdf;
+using iTextSharp.text;
+using System.IO;
+using System.Net;
 namespace PoldyCv
 {
     public partial class formBasvur : Form
@@ -68,10 +71,111 @@ namespace PoldyCv
             bgl.baglanti().Close();
             MessageBox.Show("Başvurunuz Gerçekleşmiştir Biz sizi geri ararız", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
+        public string TurkceKarakter(string text)
+        {
 
+            text = text.Replace("İ", "\u0130");
+
+            text = text.Replace("ı", "\u0131");
+
+            text = text.Replace("Ş", "\u015e");
+
+            text = text.Replace("ş", "\u015f");
+
+            text = text.Replace("Ğ", "\u011e");
+
+            text = text.Replace("ğ", "\u011f");
+
+            text = text.Replace("Ö", "\u00d6");
+
+            text = text.Replace("ö", "\u00f6");
+
+            text = text.Replace("ç", "\u00e7");
+
+            text = text.Replace("Ç", "\u00c7");
+
+            text = text.Replace("ü", "\u00fc");
+
+            text = text.Replace("Ü", "\u00dc");
+
+            return text;
+        }
         private void btnCvIndir_Click(object sender, EventArgs e)
         {
-            
+            iTextSharp.text.Document document = new iTextSharp.text.Document();
+
+            PdfWriter.GetInstance(document, new FileStream(@"C:\Users\ademk\source\repos\Poldy3\deneme\PoldyCv\Cv.pdf", FileMode.Create));
+
+            BaseFont arial = BaseFont.CreateFont("C:\\windows\\fonts\\arial.ttf", BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
+
+            iTextSharp.text.Font font = new iTextSharp.text.Font(arial, 12, iTextSharp.text.Font.NORMAL);
+
+
+
+            if (document.IsOpen() == false)
+            {
+
+                document.Open();
+
+                iTextSharp.text.Image img = iTextSharp.text.Image.GetInstance(@"C:\Users\ademk\source\repos\Poldy3\deneme\PoldyCv\vesikalik.jpg");
+                img.ScalePercent(30f, 20f);
+                //Unvanlar Çekiliyor
+                comboBox2.Items.Clear();
+                SqlCommand komut4 = new SqlCommand("Select * From Cv_Cvler Where CvMail=@p1", bgl.baglanti());
+                komut4.Parameters.AddWithValue("@p1", txtPosta.Text);
+                SqlDataReader dr4 = komut4.ExecuteReader();
+                string text = "deneme";
+                while (dr4.Read())
+                {
+                    text =
+                        "Kişisel Bilgiler \n" +
+                        "--------------------" +
+                        "\n Ad :" + dr4[1] +
+                        "\n Soyad : " + dr4[2] +
+                        "\n Doğum Tarihi : " + dr4[3] +
+                        "\n Doğum Yılı : " + dr4[4] +
+                        "\n Medeni Durum : " + dr4[5] +
+                        "\n ----------------------------------------" +
+                        "\n Eğitim Bilgileri" +
+                        "\n--------------------" +
+                        "\n Öğrenim Seviyesi :" + dr4[6] +
+                        "\n Okul Adı : " + dr4[7] +
+                        "\n Okul Başlangıç : " + dr4[8] +
+                        "\n Okul Bitiş : " + dr4[9] +
+                        "\n --------------------" +
+                        "\n Adres Bilgileri" +
+                        "\n ---------------------" +
+                        "\n Adres : " + dr4[10] +
+                        "\n --------------------" +
+                        "\n İletişim Bilgileri" +
+                        "\n ---------------------" +
+                        "\n Cep Numarası : " + dr4[11] +
+                        "\n Mail Adresi : " + dr4[12] +
+                        "\n --------------------" +
+                        "\n Yabancı Dil Bilgileri" +
+                        "\n ---------------------" +
+                        "\n Yabancı Dil : " + dr4[13] +
+                        "\n Yabancı Dil Seviye : " + dr4[14] +
+                        "\n --------------------" +
+                        "\n Yabancı Dil Bilgileri" +
+                        "\n ---------------------" +
+                        "\n Yetkinlikler : " + dr4[15] +
+                        "\n ----------------------------" +
+                        "\n Referanslar : " + dr4[15]
+                                  ;
+                }
+                bgl.baglanti().Close();
+                document.Add(img);
+
+
+
+
+                text = TurkceKarakter(text);
+                document.Add(new Paragraph(text, font));
+
+                document.Close();
+
             }
         }
+    }
     }
