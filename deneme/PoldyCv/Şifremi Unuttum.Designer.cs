f91f6031eb716550749c4212c84a314270266bff
Hasan Hüseyin <hhsonmez68@gmail.com>
2020-03-13 13:19:15 +0300

    SifremiUnuttum Son Hali

--- deneme/PoldyCv/Şifremi Unuttum.Designer.cs
+++ deneme/PoldyCv/Şifremi Unuttum.Designer.cs
@@ -63,7 +63,7 @@
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Şifremi_Unuttum";
-            this.Text = "Şifremi_Unuttum";
+            this.Text = "FormSifremiUnuttum";
             this.ResumeLayout(false);
             this.PerformLayout();
 
